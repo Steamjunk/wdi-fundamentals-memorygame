095f3ebd21d9f0c5c28cf171b3985b318da241b9
Steamjunk88 <christopher.myers88@gmail.com>
2020-07-27 15:19:17 -0500

    added js flip functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,10 +2,28 @@
 
 console.log("Up and Running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+function checkForMatch() {
+	if(cardsInPlay.length === 2) {
+		if(cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
+	}
+};
+
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch()
+};
+
+flipCard(0);
+flipCard(2);

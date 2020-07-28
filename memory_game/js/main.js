4a0498cd31f2c2498cac17bb8752c6628e5e23de
Steamjunk88 <christopher.myers88@gmail.com>
2020-07-28 09:48:47 -0500

    added js objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,7 +2,30 @@
 
 console.log("Up and Running!");
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -18,9 +41,11 @@ function checkForMatch() {
 
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
 	checkForMatch()
 };
